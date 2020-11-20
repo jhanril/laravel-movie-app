324a7aa679c833dc65af19dee24da59e1f87dc8f
Jhanril <49980166+jhanril@users.noreply.github.com>
2020-11-20 17:57:02 +0800

    first commit

---
+++ app/Http/Livewire/SearchDropdown.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Http;
+
+
+class SearchDropdown extends Component
+{
+    public $search = '';
+
+    public function render()
+    {
+        $searchResults = [];
+
+        //if(strlen($this->search) > 2)
+
+        if ($this->search!='') {
+
+            $searchResults = Http::withToken(config('services.tmdb.token'))
+                ->get('https://api.themoviedb.org/3/search/movie?query='.$this->search)
+                ->json()['results'];
+        }
+
+        return view('livewire.search-dropdown', [
+            'searchResults' => $searchResults,
+        ]);
+
+    }
+}
---
+++ resources/views/livewire/search-dropdown.blade.php
@@ -0,0 +1,35 @@
+<div class="relative mt-3 md:mt-0" x-data="{isOpen: true}" @click.away="isOpen = false">
+    <input wire:model.debounce.200ms="search" type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search" @focus="isOpen = true" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false" @keydown="isOpen = true">
+    <div class="absolute top-0">
+        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
+    </div>
+
+    @if (strlen($search) >= 2)
+        
+        <div class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4" x-show.transition.opacity="isOpen" @keydown.escape.window="isOpen = false">
+            @if (!empty($searchResults))
+                <ul class="scrolling-auto overflow-auto h-64">
+                    @foreach ($searchResults as $result)
+                        
+                        <li class="border-b border-gray-700">
+                            <a href="{{ route('movies.show', $result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center" @if ($loop->last) @keydown.tab="isOpen = false" @endif>
+
+                            @if ($result['poster_path'])
+                                <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-8">
+                            @else
+                                <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                            @endif
+                                
+                            <span class="ml-4">{{ $result['title'] }}</span>
+                            </a>
+                        </li>
+                    @endforeach
+
+                </ul>
+            @else
+                <div class="px-3 py-3">No results for "{{ $search }}"</div>
+            @endif
+        </div>
+
+    @endif
+</div>
